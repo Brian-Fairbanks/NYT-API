0a6f66dc9abb4cf7309c9698e92145e065dadbca
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-27 11:35:39 -0500

    added js file to html.  Added then call to ajax.

--- logic.js
+++ logic.js
@@ -3,4 +3,7 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 $.ajax({
   url: queryURL,
   method: "GET"
+}).then(function(response){
+  alert("running api call")
+  $("#well-section").append(JSON.stringify(response))
 })
